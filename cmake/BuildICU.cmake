6b486f2368a361c70b2b837c87b9aec3e8395aed
Alexandros Roussos <thealexroussos@gmail.com>
2018-12-23 22:48:38 +0100

    Fixed build for r19 beta NDK

--- cmake/BuildICU.cmake
+++ cmake/BuildICU.cmake
@@ -89,43 +89,20 @@ add_dependencies(icu icu_host)
 
 if (ICU_CROSS_ARCH)
     if (ANDROID)
-        # copy over both sysroots to a common sysroot (workaround ICU failing without one single sysroot)
-        file(COPY ${ANDROID_SYSTEM_LIBRARY_PATH}/usr DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/sysroot/)
-        file(COPY ${CMAKE_SYSROOT}/usr/include DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/sysroot/usr/)
-
-        # for c++ standard headers
-        set(CROSS_INCLUDES "")
-        foreach(INC ${CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES})
-            set(CROSS_INCLUDES "${CROSS_INCLUDES} -I${INC}")
-        endforeach()
+        # manually set up linker search path
+        list(GET CMAKE_SYSTEM_LIBRARY_PATH 0 ANDROID_SYS_LIB_PATH)
+        set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -L${ANDROID_SYS_LIB_PATH}")
         
-        # for c++ standard libs
-        set(CROSS_LIBS ${CMAKE_C_STANDARD_LIBRARIES_INIT})
-        string(REPLACE "\" \"" ";" ANDROID_CXX_STANDARD_LIBRARIES ${ANDROID_CXX_STANDARD_LIBRARIES})
-        string(REPLACE "\"" "" ANDROID_CXX_STANDARD_LIBRARIES ${ANDROID_CXX_STANDARD_LIBRARIES})
-        foreach(LIB ${ANDROID_CXX_STANDARD_LIBRARIES})
-            get_filename_component(LIB_PATH ${LIB} DIRECTORY)
-            get_filename_component(LIB_NAME ${LIB} NAME_WE)
-            # remove starting "lib"
-            string(REGEX REPLACE "^lib" "" LIB_NAME ${LIB_NAME})
-            
-            set(CROSS_LIBS "${CROSS_LIBS} -L${LIB_PATH} -l${LIB_NAME}")
-        endforeach()
-
-        set(CROSS_CFLAGS "")
-        # fix http://bugs.icu-project.org/trac/ticket/12854
-        set(CROSS_CXXFLAGS "-DU_HAVE_XLOCALE_H=0")
-        set(CROSS_CC "${CMAKE_C_COMPILER} ${CMAKE_C_COMPILE_OPTIONS_EXTERNAL_TOOLCHAIN}${CMAKE_C_COMPILER_EXTERNAL_TOOLCHAIN} --sysroot=${CMAKE_CURRENT_BINARY_DIR}/sysroot ${CMAKE_C_FLAGS} -target ${CMAKE_C_COMPILER_TARGET}")
-        set(CROSS_CXX "${CMAKE_CXX_COMPILER} ${CMAKE_CXX_COMPILE_OPTIONS_EXTERNAL_TOOLCHAIN}${CMAKE_CXX_COMPILER_EXTERNAL_TOOLCHAIN} --sysroot=${CMAKE_CURRENT_BINARY_DIR}/sysroot ${CMAKE_CXX_FLAGS} ${CROSS_INCLUDES} -target ${CMAKE_CXX_COMPILER_TARGET}")
-        set(CROSS_LDFLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${CROSS_LIBS}")
-    else()
-        set(CROSS_CFLAGS "${CMAKE_C_FLAGS}")
-        set(CROSS_CXXFLAGS "${CMAKE_CXX_FLAGS}")
-        set(CROSS_CC "${CMAKE_C_COMPILER}")
-        set(CROSS_CXX "${CMAKE_CXX_COMPILER}")
-        set(CROSS_LDFLAGS "${CMAKE_MODULE_LINKER_FLAGS}")
+        # manually set up llvm target triple
+        set(CMAKE_C_COMPILER "${CMAKE_C_COMPILER} -target ${ANDROID_LLVM_TRIPLE}")
+        set(CMAKE_CXX_COMPILER "${CMAKE_CXX_COMPILER} -target ${ANDROID_LLVM_TRIPLE}")
     endif()
-
+    set(CROSS_CFLAGS "${CMAKE_C_FLAGS}")
+    set(CROSS_CXXFLAGS "${CMAKE_CXX_FLAGS}")
+    set(CROSS_CC "${CMAKE_C_COMPILER}")
+    set(CROSS_CXX "${CMAKE_CXX_COMPILER}")
+    set(CROSS_LDFLAGS "${CMAKE_MODULE_LINKER_FLAGS}")
+    
     set(CROSS_ENV_CMAKE ${CMAKE_COMMAND} -E env
             CC=${CROSS_CC}
             CXX=${CROSS_CXX}
@@ -142,8 +119,6 @@ if (ICU_CROSS_ARCH)
             DEPENDS icu_host
             SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/icu
             BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/icu_cross-build
-            PATCH_COMMAND ${PATCH_PROGRAM} -p1 --forward -r - < ${CMAKE_CURRENT_SOURCE_DIR}/patches/0020-workaround-missing-locale.patch || true
-            COMMAND ${PATCH_PROGRAM} -p1 --forward -r - < ${CMAKE_CURRENT_SOURCE_DIR}/patches/0022-fix-namespace.patch || true
             CONFIGURE_COMMAND ${CROSS_ENV_CMAKE} sh <SOURCE_DIR>/source/configure --enable-static --prefix=${CMAKE_CURRENT_BINARY_DIR}/icu_cross
             --libdir=${CMAKE_CURRENT_BINARY_DIR}/icu_cross/lib/ --host=${ICU_CROSS_ARCH} --with-cross-build=${CMAKE_CURRENT_BINARY_DIR}/icu_host-build
             BUILD_COMMAND ${CROSS_ENV_CMAKE} ${MAKE_PROGRAM} -j ${NUM_JOBS}
